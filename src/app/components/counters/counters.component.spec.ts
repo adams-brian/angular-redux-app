c1fd63e2cfd1f9e482ff43e58a43e246f16dbfed
Brian Adams <brian.lives.outdoors@gmail.com>
2017-11-01 21:04:06 -0400

    add Couters tests

--- src/app/components/counters/counters.component.spec.ts
+++ src/app/components/counters/counters.component.spec.ts
@@ -1,22 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { StoreModule} from '@ngrx/store';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { StoreModule, Store } from '@ngrx/store';
 
 import { CountersComponent } from './counters.component';
 import { CounterComponent } from '../counter/counter.component';
-import { countersReducer } from '../../store';
+import { countersReducer, AppState, AddCounter, RemoveCounter } from '../../store';
 
-describe('CountersComponent', () => {
+fdescribe('CountersComponent', () => {
   let component: CountersComponent;
   let fixture: ComponentFixture<CountersComponent>;
+  let store: Store<AppState>;
+  const initialState = {
+    counters: [ 3, 1, 5, 2 ]
+  };
+  let addButton: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ 
+      declarations: [
         CountersComponent,
         CounterComponent
       ],
       imports: [
-        StoreModule.forRoot({ counters: countersReducer })
+        StoreModule.forRoot(
+          { counters: countersReducer },
+          { initialState: initialState }
+        )
       ]
     })
     .compileComponents();
@@ -25,10 +35,44 @@ describe('CountersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CountersComponent);
     component = fixture.componentInstance;
+
+    store = fixture.debugElement.injector.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+
+    addButton = fixture.debugElement.query(By.css('.add-counter'));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the correct number of counters', () => {
+    expect(fixture.debugElement.queryAll(By.css('app-counter')).length).toBe(4);
+  });
+
+  it('should dispatch an ADD_COUNTER action when add is clicked', () => {
+    addButton.triggerEventHandler('click', null);
+    expect(store.dispatch).toHaveBeenCalledWith(new AddCounter());
+  });
+
+  it('should show the correct number of counters', () => {
+    const counters = fixture.debugElement.queryAll(By.css('app-counter'));
+    expect(counters.length).toBe(4);
+  });
+
+  it('should respond to ADD_COUNTER actions', () => {
+    store.dispatch(new AddCounter());
+    store.dispatch(new AddCounter());
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('app-counter')).length).toBe(6);
+  });
+
+  it('should respond to REMOVE_COUNTER actions', () => {
+    store.dispatch(new RemoveCounter(0));
+    store.dispatch(new RemoveCounter(0));
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('app-counter')).length).toBe(2);
   });
 });
